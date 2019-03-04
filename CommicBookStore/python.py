cb5c0d8c3afbd7837c0a449201e5c7952f0576d9
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-03-05 10:05:46 +1300

    Added index page and store page
    Index page is void 
    Store pages displays each book as a card with appropriate data

--- CommicBookStore/python.py
+++ CommicBookStore/python.py
@@ -1,8 +1,8 @@
 from bottle import run, route, view, get, post, request, static_file
 from itertools import count
 
-
-class Books:
+class Book:
+    _ids = count (0)
     name = "How the heck would I know"
     description = "It seems no one cared enough to write a description."
     cost = "At least $0"
@@ -15,6 +15,7 @@ class Books:
                  cost = "At least $0",
                  stock = "Probably none",
                  photo = "Void"):
+        self.id = next(self._ids)
         self.name = name
         self.description = description
         self.cost = cost
@@ -23,10 +24,34 @@ class Books:
         
     
 books = [
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "images/TintinInAmerica")
+    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
+    Book(name= "Tintin and the Picaros", cost= 25, stock= 5, photo= "https://images-na.ssl-images-amazon.com/images/I/61%2Bp-97QRIL.jpg"),
+    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
+    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
+    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
+    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg")
 ]
+
+
+#Index page
+@route('/')
+@view('index')
+def index():
+    pass
+
+#Store page
+@route('/store')
+@view('store')
+def store():
+    data = dict(books_list = books)
+    return data
+
+#Cart updated page
+@route('/cart_updated', METHOD="POST")
+@view('cart_updated')
+def cart_updated():
+    pass
+
+
+
+run(host='0.0.0.0', port = 6969, reloader = True, debug = True)
