ed5096238e11bb655d2a632e9baf027c8db2ce77
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-03-06 15:10:17 +1300

    Add files via upload

--- CommicBookStore/python.py
+++ CommicBookStore/python.py
@@ -33,12 +33,12 @@ class Book:
         
     
 books = [
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
-    Book(name= "Tintin and the Picaros", cost= 25, stock= 5, photo= "https://images-na.ssl-images-amazon.com/images/I/61%2Bp-97QRIL.jpg"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
-    Book(name= "Tintin in America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg")
+    Book(name= "Tintin In America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
+    Book(name= "Tintin And The Picaros", cost= 25, stock= 5, photo= "https://images-na.ssl-images-amazon.com/images/I/61%2Bp-97QRIL.jpg"),
+    Book(name= "Pristoners Of The Sun", cost= 23, stock= 11983, photo= "https://www.sbs.com.au/guide/sites/sbs.com.au.guide/files/styles/body_image/public/tt_14.jpg?itok=nocJ7UhB&mtime=1510616903"),
+    Book(name= "The Seven Crystal Balls", cost= 50, stock= 2, photo= "https://www.dimanoinmano.it/img/166498/full/fumetti/fumetti-classici/tintin-the-seven-crystal-balls.jpg"),
+    Book(name= "The Shooting Star", cost= 20, stock= 8, photo= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDn2tOeZ0djqsMmrRolQWMnly6CtsElg3nxY3g-uT1eVlpJ8i4Q"),
+    Book(name= "Tintin In The Congo", cost= 30, stock= 10, photo= "http://en.tintin.com/images/tintin/actus/actus/004442/tintin-Congo_coverEN.jpg")
 ]
 
 users = [
