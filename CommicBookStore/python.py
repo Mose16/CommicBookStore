78c01a12b58471f179bb5fbd06a37bf9618150f3
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-03-12 10:36:55 +1300

    Add files via upload

--- CommicBookStore/python.py
+++ CommicBookStore/python.py
@@ -3,11 +3,15 @@ from itertools import count
 
 
 class User:
+    _ids = count (0)
     books_in_cart = []
     name = None
+    password = None
     
-    def __init__(self, name):
+    def __init__(self, name, password):
+        self.id = next(self._ids)
         self.name = name
+        self.password = password
     
     
 class Book:
@@ -33,19 +37,29 @@ class Book:
         
     
 books = [
-    Book(name= "Tintin In America", cost= 25, stock= 7, photo= "https://images-na.ssl-images-amazon.com/images/I/912vIXwG23L.jpg"),
-    Book(name= "Tintin And The Picaros", cost= 25, stock= 5, photo= "https://images-na.ssl-images-amazon.com/images/I/61%2Bp-97QRIL.jpg"),
-    Book(name= "Pristoners Of The Sun", cost= 23, stock= 11983, photo= "https://www.sbs.com.au/guide/sites/sbs.com.au.guide/files/styles/body_image/public/tt_14.jpg?itok=nocJ7UhB&mtime=1510616903"),
-    Book(name= "The Seven Crystal Balls", cost= 50, stock= 2, photo= "https://www.dimanoinmano.it/img/166498/full/fumetti/fumetti-classici/tintin-the-seven-crystal-balls.jpg"),
-    Book(name= "The Shooting Star", cost= 20, stock= 8, photo= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfDn2tOeZ0djqsMmrRolQWMnly6CtsElg3nxY3g-uT1eVlpJ8i4Q"),
-    Book(name= "Tintin In The Congo", cost= 30, stock= 10, photo= "http://en.tintin.com/images/tintin/actus/actus/004442/tintin-Congo_coverEN.jpg")
+    Book(name= "Tintin In America", cost= 25, stock= 7, photo= "TintinInAmerica.jpg"),
+    Book(name= "Tintin And The Picaros", cost= 25, stock= 5, photo= "TintinAndThePicaros.jpg"),
+    Book(name= "Prisoners Of The Sun", cost= 23, stock= 11983, photo= "PrisonersOfTheSun.jpg"),
+    Book(name= "The Seven Crystal Balls", cost= 50, stock= 2, photo= "TheSevenCrystalBalls.jpg"),
+    Book(name= "The Shooting Star", cost= 20, stock= 8, photo= "TheShootingStar.jpg"),
+    Book(name= "Tintin In The Congo", cost= 30, stock= 10, photo= "TintinInTheCongo.jpg")
 ]
 
 users = [
-    User("Moses")
+    User("Moses", "Lel"),
+    User("Jeremey", "PooBumy69")
 ]
 
 loged_user = users[0]
+
+
+#Images
+@route('/pictures/<filename>')
+def upload_image(filename):
+    return static_file(filename, root='./images')
+
+
+###Pages
 
 #Index page
 @route('/')
@@ -64,15 +78,40 @@ def store():
 @route('/sign_in')
 @view('sign_in')
 def sign_in():
-    data = dict(users_list = users)
-    return data
-
-#Sign_in_success
-@route('/sign_in_success')
-@view('sign_in_success')
-def sign_in_success():
     pass
 
+#Sign_in_success
+@route('/sign_in_success', method = "POST")
+@view('sign_in_success')
+def sign_in_success():
+    found_user = None
+    for u in users:
+        if request.forms.get("name") == u.name:
+            if request.forms.get("password") == u.password:
+                found_user = u
+                loged_user = found_user
+                return dict(user = loged_user)
+        
+
+    
+    
+    
+
+"""
+#Sign_up_success
+@route('/sign_up_success', method = "POST")
+@view('sign_up_success')
+def sign_up_success():
+    name = request.forms.get("name")
+    password = request.forms.get("password")
+    
+    users.append(User(name, password))"""
+
+#Cart page
+@route('/cart')
+@view('cart')
+def cart():
+    return dict(cart = loged_user.books_in_cart)
 
 #Cart updated page
 @route('/cart_updated/<book_id>')
@@ -85,10 +124,10 @@ def cart_updated(book_id):
             found_book = book
             break
     found_book.stock -= 1
-    data = dict(book = found_book)
     loged_user.books_in_cart.append(found_book)
-    return data
+    return dict(book = found_book)
 
 
 
 run(host='0.0.0.0', port = 6969, reloader = True, debug = True)
+
