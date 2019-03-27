06a6cb907df3555b0aeaf5b14b2f024711572901
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-03-27 17:50:30 +1300

    Add files via upload

--- CommicBookStore/generator.py
+++ CommicBookStore/generator.py
@@ -5,12 +5,13 @@ file = open("python.txt", "r")
 xPass = True
 xAnd = True
 xQuotes = False
-xConditionals = True
+xFuncCheck = True
 
 
 
 
 ###---Functions---###
+#File managers
 def readFileLines(file):
     lines = file.readlines()
     return lines     
@@ -19,6 +20,30 @@ def createFile(fileName, text):
     newFile = open(fileName + ".txt", "w+")
     newFile.write(text)
     return
+
+
+#File editors
+def functionCheck(line):
+    global classSkip
+    if line.find("def ") == -1:
+        for function in functions:
+            if line.find("to " + function + "(") != -1:
+                pos = line.find(function + "(")
+                line = line[:pos] + "the return of function " + line[pos:]                
+            elif line.find(function + "(") != -1 and line.find("called " + function + "(") == -1:
+                pos = line.find(function + "(")
+                line = line[:pos] + "Call function " + line[pos:]
+                        
+    if line.find("class ") == -1:
+        for class_ in classes:
+            if line.find("to " + class_ + "(") != -1:
+                pos = line.find(class_ + "(")
+                line = line[:pos] + "an iteration of class " + line[pos:]                
+            elif line.find(class_ + "(") != -1:
+                pos = line.find(class_ + "(")
+                line = line[:pos] + "an iteration of class " + line[pos:]    
+                         
+    return line
 
 def addPass(line):
     if line.find("def ") != -1:
@@ -31,7 +56,7 @@ def addPass(line):
     if line.find("()") == -1:
         if line.find("(") != -1:
             pos = line.find("(") + 1
-            line = line[:pos] + "Passing variables: " + line[pos:]
+            line = line[:pos] + "Passing parameters: " + line[pos:]
     return line 
 
 def repAnd(line):
@@ -54,7 +79,7 @@ def wordRep(line):
             if line.find(word) != -1:
                 pos = line.find(word)
                 spaces = len(line) - len(line.lstrip())
-                line = (" " * spaces) + line[:spaces] + PSEUDO[word] + line[spaces:pos] + "' to: " + line[pos + len(word):]
+                line = (" " * spaces) + line[:spaces] + PSEUDO[word] + line[spaces:pos] + "' to " + line[pos + len(word):]
         elif word == "def " or word == "class " or word == "def __init__":
             if line.find(word) != -1:
                 ###Add to arrays###
@@ -76,11 +101,10 @@ def wordRep(line):
                         end = line.find(", ", pos)
                     else:
                         end = line.find("):", pos)
-                    line =  line[:start + 2] + "(Set variable '" + line[start + 2:pos] + "' to " + line[pos + 3:end] + " by default)" + line[end:]
+                    line =  line[:start + 2] + "(Set parameter '" + line[start + 2:pos] + "' to " + line[pos + 3:end] + " by default)" + line[end:]
         else:
             if line.find(word) != -1:
                 pos = line.find(word)
-                
                 line = line[:pos] + PSEUDO[word] + line[pos + len(word):]                
 
     return line
@@ -93,15 +117,14 @@ PSEUDO = {
     "def ":"Define a function called ",
     "class ":"Define a class called ",
     " = ":"Set variable '",
-    "pass":"Do nothing ",
+    "pass ":"Do nothing ",
     "def __init__":"Define object constructor function",
     "==":" equal to ",
-    "<":" less than ",
-    ">":" greater than ",
-    
-    "<=":" less than or equal to ",
-    ">=":" greater than or equal to ",
-    "!=":" not equal to ",    
+    "<":" is less than ",
+    ">":" is greater than ",
+    "<=":" is less than or equal to ",
+    ">=":" is greater than or equal to ",
+    "!=":" is not equal to ",    
 }
 
 WORDS = [
@@ -109,11 +132,13 @@ WORDS = [
     "def ",
     "class ",
     " = ",
-    "pass",
+    "pass ",
     "==",
     "<=",
     ">=",
-    "!="
+    "!=",
+    ">",
+    "<"    
 ]
 
 functions = []
@@ -126,7 +151,7 @@ classes = []
 ###-------Main Code-------###
 lines = readFileLines(file)
 newFile = ""
-for line in lines:    
+for line in lines:  
     #Conditionals
     if xPass:
         line = addPass(line)
@@ -135,12 +160,25 @@ for line in lines:
         line = repAnd(line)
     if xQuotes:
         line = removeQuotes(line)
-    
+        
     newFile += line
+    
+createFile("generatedFile", newFile)
+lines = readFileLines(open("generatedFile.txt", "r"))
+newFile = ""
+
+if xFuncCheck:
+    for line in lines:
+        line = functionCheck(line)    
+        if xPass:
+            line = addPass(line)  
+        newFile += line
+    
+createFile("generatedFile", newFile)
+    
 
 print(newFile)
 print(classes, functions)
 
 
 
-createFile("generatedFile", newFile)
